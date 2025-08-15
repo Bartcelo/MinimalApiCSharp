b12a61275987f40a9e92ab44dc8c9c088c32f6cb
marcelo <bart.celo@hotmail.com>
2025-08-15 10:54:51 -0300

    Conexao com o banco de dados e migrations criadas com sucesso

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -2,11 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Minimal_Api.Dominio.Entidades;
 
 namespace Minimal_Api.Infraestrutura.Db
 {
-    public class DbContexto
+    public class DbContexto : DbContext
     {
-        
+        private readonly IConfiguration _configurationAppSetings;
+
+        public DbContexto(IConfiguration configurationAppSetings)
+        {
+            _configurationAppSetings = configurationAppSetings;
+        }
+
+
+        public DbSet<Administrador> Administradores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                var stringConexao = _configurationAppSetings.GetConnectionString("mysql").ToString();
+                if (!string.IsNullOrEmpty(stringConexao))
+                {
+                    optionsBuilder.UseMySql(
+                    stringConexao,
+                    ServerVersion.AutoDetect(stringConexao)
+                );
+                }
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Minimal_Api.Dominio.DTOs;
+using Minimal_Api.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>{
+options.UseMySql(
+    builder.Configuration.GetConnectionString("mysql"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
 app.MapPost("/login", (LoginDto loginDto) =>
-loginDto.Email == "bart.celo@hotmail.com" && loginDto.Password == "123456"  
-    ? Results.Ok("Login realizado com sucesso") 
+loginDto.Email == "bart.celo@hotmail.com" && loginDto.Password == "123456"
+    ? Results.Ok("Login realizado com sucesso")
     : Results.BadRequest("Acesso negado")
 );
 
